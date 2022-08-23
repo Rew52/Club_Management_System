de3d6bd4cf3cd3f21dd0295c7467522e0217bcef
Rew52 <111581504+Rew52@users.noreply.github.com>
2022-08-23 19:27:18 +0700

    Update A_EventM.cs

--- ASM2DDD/A_EventM.cs
+++ ASM2DDD/A_EventM.cs
@@ -61,10 +61,15 @@ namespace ASM2DDD
 
         private void btLogout_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            this.Close();
-            Login login_form = new Login();
-            login_form.ShowDialog();
+            if (MessageBox.Show("Want to exit your account?", "",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {   // Close current page.
+                this.Close();
+                // Open the page you want to go to.
+                this.Hide();
+                Login login_form = new Login();
+                login_form.ShowDialog();
+            }
         }
 
         private void btRemove_Click(object sender, EventArgs e)// After clicking "Refresh Input" the program will run the following commands.
