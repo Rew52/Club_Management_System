bc874ca16aaab93d6ab3709902f75dc3a0475dbc
Rew52 <111581504+Rew52@users.noreply.github.com>
2022-08-23 19:31:23 +0700

    Update Admin_Choose.cs

--- ASM2DDD/Admin_Choose.cs
+++ ASM2DDD/Admin_Choose.cs
@@ -42,12 +42,15 @@ namespace ASM2DDD
 
         private void button1_Click(object sender, EventArgs e) //After clicking "Log out" the program will run the following commands.
         {
-            // Close current page.
-            this.Close();
-            // Back to account login page.
-            this.Hide();
-            Login login_form = new Login();
-            login_form.ShowDialog();
+            if (MessageBox.Show("Want to exit your account?", "",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {   // Close current page.
+                this.Close();
+                // Open the page you want to go to.
+                this.Hide();
+                Login login_form = new Login();
+                login_form.ShowDialog();
+            }
         }
 
         private void btManageEvent_Click(object sender, EventArgs e) //After clicking "Log Out" the program will run the following commands
